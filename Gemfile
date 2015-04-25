54e834968e52487d340d6e3bf7752e7b39d1e1f7
DanFerrer <danielferrer212@gmail.com>
2015-04-25 19:48:15 -0400

    added youtubeloader

--- Gemfile
+++ Gemfile
@@ -19,6 +19,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'httparty'
+gem 'google-api-client'
+gem 'trollop', '~> 2.1.2'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/migrate/20150422015422_devise_create_users.rb
+++ db/migrate/20150422015422_devise_create_users.rb
@@ -1,6 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :name, null: false
+      t.string :username, null: false
+      t.string :image_url
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
